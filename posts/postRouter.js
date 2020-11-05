5ae6a55a00e9911b547f54fd912b12a99c6de9b4
Michael Gregory <michael.gregory8584@gmail.com>
2020-11-04 19:44:47 -0600

    userRouter complete

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -18,22 +18,39 @@ router.get('/', (req, res) => {
   })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const {id} = req.params;
+  posts.getById(id)
+  .then(post => {
+    if (post) {
+      req.post = post;
+      next()
+    } else {
+      res.status(404).json({
+        message:"post not found"
+      })
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      message:"a database error occured"
+    })
+  })
 }
 
 module.exports = router;
